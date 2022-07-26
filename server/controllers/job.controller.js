a751559d42d987e94f0f0be42d2282a945e07489
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-26 15:27:37 +0700

    #10 - Create a Job

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -1,13 +1,24 @@
+const Job = require("../models/Job.model");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+const createJob = async (req, res) => {
+  try {
+    req.body.createdBy = req.user.userId;
+    const newJob = await Job.create(req.body);
+    return res.status(StatusCodes.CREATED).json({ newJob });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
 const getAllJobs = async (req, res) => {
   return res.send("getAllJobs");
 };
 
 const getSingleJob = async (req, res) => {
   return res.send("getSingleJob");
-};
-
-const createJob = async (req, res) => {
-  return res.send("createJob");
 };
 
 const updateJob = async (req, res) => {
