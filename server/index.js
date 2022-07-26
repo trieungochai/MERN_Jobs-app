d095a55601e45a3096622f991235441df1511c37
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-26 19:04:51 +0700

    #15 Setup Security

--- server/index.js
+++ server/index.js
@@ -7,12 +7,27 @@ const jobsRouter = require("./routes/jobs.router");
 const connectDB = require("./db/connect");
 const verifyToken = require("./middleware/auth.middleware");
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 // error handler/ middleware
 const errorHandlerMiddleware = require("./middleware/error-handler.middleware");
 const notFoundMiddleware = require("./middleware/not-found.middleware");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 request per windows
+  })
+);
 app.use(express.json());
-
+app.use(cors());
+app.use(xss());
+app.use(helmet());
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", verifyToken, jobsRouter);
