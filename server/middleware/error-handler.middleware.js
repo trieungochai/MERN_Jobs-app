f63e0e9b6c7019064e3c38cb97a36fd28758585d
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-26 17:44:53 +0700

    #13 Custom Error

--- server/middleware/error-handler.middleware.js
+++ server/middleware/error-handler.middleware.js
@@ -8,12 +8,14 @@ const errorHandlerMiddleware = (error, req, res, next) => {
     message: error.message || "Something went wrong, please try again later",
   };
 
-  if (error instanceof CustomAPIError) {
-    return res.status(error.StatusCodes).json({ message: error.message });
-  }
+  // if (error instanceof CustomAPIError) {
+  //   return res.status(error.StatusCodes).json({ message: error.message });
+  // }
 
   if (error.code && error.code === 11000) {
-    customError.message = `Duplicate value entered for ${Object.keyValue} field, please choose another value`;
+    customError.message = `Duplicate value entered for ${Object.keys(
+      error.keyValue
+    )} field, please choose another value`;
     customError.statusCode = 400;
   }
   return res
