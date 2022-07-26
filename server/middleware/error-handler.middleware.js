bf6026a99dce2b4d2d9120fef555f32bb63a9eac
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-26 17:42:12 +0700

    #13 Duplicate Error

--- server/middleware/error-handler.middleware.js
+++ server/middleware/error-handler.middleware.js
@@ -2,10 +2,23 @@ const { StatusCodes } = require("http-status-codes");
 const { CustomAPIError } = require("../errors");
 
 const errorHandlerMiddleware = (error, req, res, next) => {
+  let customError = {
+    // set default
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: error.message || "Something went wrong, please try again later",
+  };
+
   if (error instanceof CustomAPIError) {
     return res.status(error.StatusCodes).json({ message: error.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+
+  if (error.code && error.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keyValue} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+  return res
+    .status(customError.statusCode)
+    .json({ message: customError.message });
 };
 
 module.exports = errorHandlerMiddleware;
